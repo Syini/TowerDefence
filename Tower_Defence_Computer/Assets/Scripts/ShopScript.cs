10292100779960c742495dd5c08afe2e562f01f6
Syini <shdyruk@gmail.com>
2017-12-27 01:32:56 +0200

    Update ShopScript.cs

--- Tower_Defence_Computer/Assets/Scripts/ShopScript.cs
+++ Tower_Defence_Computer/Assets/Scripts/ShopScript.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class ShopScript : MonoBehaviour {
-//	public GameObject ItemPref;
-//	public Transform ItemGrid;
-//	// Use this for initialization
-//	void Start () {
-//		
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		
-//	}
+	public GameObject ItemPref;
+	public Transform ItemGrid;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
